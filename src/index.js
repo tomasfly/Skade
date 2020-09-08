6d537518fc565ffa50dbb54daefe082952e7d75a
Tomas Fleiderman <tomasfleiderman@xpertai.com>
2020-09-08 11:14:16 +0200

    adding new functions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 const AlphaVantage = require('../lib/AlphaVantage')
 
 ab = new AlphaVantage()
-    ab.getDailyPrice('tsla')
-    // ab.getIntraDayAdvanced('aapl', 'full', 'json', '60min')
-// ab.backTest();
+
+// const instrumentsArray = ['TSLA','MRNA','NVDA','PINS','AMZN','SNAP','SPOT','AAPL','YNDX','NTDOY','FB','CRM','ADBE','APHA','LUV','EBAY','WSM','NFLX','BABA','SVMK','MSFT','SIE','ATVI','TCTZ','AA','NKE','RACE','DAI','CAT','SAP','GOOGL','RYAAY','DBK','TWTR','SQM','VALE','HD','ORCL','NVTK','MA','DIS','SBUX','PG','EL','V','SK','RCL','UNH','GS','ETH','ADS','BIDU','MCD','JUVE','BMW','F','VZ','WEED','HOG','UBER','MTS','TM','JNJ','PFE','RNO','BAC','CSCO','LNVG','TRIP','DBX','VOW','SBER','BAS','AXP','VFC','BNP','IBM','JPM','AFLT','OGZPY','CRON','ROSN','ENEL','TRV','WYNN','KO','TIF','GMKN','PVH','PM','DG','HPQ','INTC','BSAC','PTR','FP','CHL','LUKOY','ITX','C','ENIC','BAYN','PBR','GILD','XOM','LYFT','SAVE','VOD','T','IDCB','KORS','ENI','AIR','RL','EDF','GE','CCL','NCLH','REP','WFC','AF','BA','TUI','ACB','TLRY','JWN','LTM']
+
+const instrumentsArray = ['MRNA']
+const differences = []
+
+async function techAnalysis() {
+    let diff
+    for (const element of instrumentsArray) {
+        diff = await ab.getMACDDifference(element);
+        differences.push({ symbol: element, difference: diff })
+    }
+    console.log(differences)
+}
+
+techAnalysis()
+
