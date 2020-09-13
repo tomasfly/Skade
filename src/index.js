c6a2b17cb3ed5166b921a4ca6d3c32f83c418cdc
Tomas Fleiderman <tomasfleiderman@xpertai.com>
2020-09-13 12:38:14 +0200

    updating branch

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const AlphaVantage = require('../lib/AlphaVantage')
 const fs = require('fs')
 const logger = require('../helper/logger')
+const moment = require('moment')
 
 ab = new AlphaVantage()
 
 // const instrumentsArray = ['TSLA','MRNA','NVDA','PINS','AMZN','SNAP','SPOT','AAPL','YNDX','NTDOY','FB','CRM','ADBE','APHA','LUV','EBAY','WSM','NFLX','BABA','SVMK','MSFT','SIE','ATVI','TCTZ','AA','NKE','RACE','DAI','CAT','SAP','GOOGL','RYAAY','DBK','TWTR','SQM','VALE','HD','ORCL','NVTK','MA','DIS','SBUX','PG','EL','V','SK','RCL','UNH','GS','ETH','ADS','BIDU','MCD','JUVE','BMW','F','VZ','WEED','HOG','UBER','MTS','TM','JNJ','PFE','RNO','BAC','CSCO','LNVG','TRIP','DBX','VOW','SBER','BAS','AXP','VFC','BNP','IBM','JPM','AFLT','OGZPY','CRON','ROSN','ENEL','TRV','WYNN','KO','TIF','GMKN','PVH','PM','DG','HPQ','INTC','BSAC','PTR','FP','CHL','LUKOY','ITX','C','ENIC','BAYN','PBR','GILD','XOM','LYFT','SAVE','VOD','T','IDCB','KORS','ENI','AIR','RL','EDF','GE','CCL','NCLH','REP','WFC','AF','BA','TUI','ACB','TLRY','JWN','LTM']
 
-const instrumentsArray = ['TSLA', 'MRNA', 'NVDA', 'PINS', 'AMZN', 'SNAP', 'SPOT', 'AAPL', 'YNDX', 'NTDOY', 'FB', 'CRM', 'ADBE', 'APHA', 'LUV', 'EBAY', 'WSM', 'NFLX', 'BABA', 'SVMK', 'MSFT', 'SIE', 'ATVI', 'AA', 'NKE', 'RACE', 'DAI', 'CAT', 'SAP', 'GOOGL', 'RYAAY', 'DBK', 'TWTR', 'SQM', 'VALE', 'HD', 'ORCL', 'MA', 'DIS', 'SBUX', 'PG', 'EL', 'V', 'SK', 'RCL', 'UNH', 'GS', 'ETH', 'ADS', 'BIDU', 'MCD', 'BMW', 'F', 'VZ', 'HOG', 'UBER', 'MTS', 'TM', 'JNJ', 'PFE', 'RNO', 'BAC', 'CSCO', 'TRIP', 'DBX', 'VOW', 'BAS', 'AXP', 'VFC', 'BNP', 'IBM', 'JPM', 'OGZPY', 'CRON', 'ROSN', 'TRV', 'WYNN', 'KO', 'TIF', 'PVH', 'PM', 'DG', 'HPQ', 'INTC', 'BSAC', 'PTR', 'FP', 'CHL', 'LUKOY', 'ITX', 'C', 'ENIC']
+const instrumentsArray = ['TSLA', 'MRNA', 'NVDA', 'PINS', 'AMZN', 'SNAP', 'SPOT', 'AAPL', 'YNDX', 'NTDOY', 'FB', 'CRM', 'ADBE', 'APHA', 'LUV', 'EBAY', 'WSM', 'NFLX', 'BABA', 'SVMK', 'MSFT', 'SIE', 'ATVI', 'TCTZ', 'AA', 'NKE', 'RACE', 'DAI', 'CAT', 'SAP', 'GOOGL', 'RYAAY', 'DBK', 'TWTR', 'SQM', 'VALE', 'HD', 'ORCL', 'NVTK', 'MA', 'DIS', 'SBUX', 'PG', 'EL', 'V', 'SK', 'RCL', 'UNH', 'GS', 'ETH', 'ADS', 'BIDU', 'MCD', 'JUVE', 'BMW', 'F', 'VZ', 'WEED', 'HOG', 'UBER', 'MTS', 'TM', 'JNJ', 'PFE', 'RNO', 'BAC', 'CSCO', 'LNVG', 'TRIP', 'DBX', 'VOW', 'SBER', 'BAS', 'AXP', 'VFC', 'BNP', 'IBM', 'JPM', 'AFLT', 'OGZPY', 'CRON', 'ROSN', 'ENEL', 'TRV', 'WYNN', 'KO', 'TIF', 'GMKN']
+
 const returnArray = []
 
 // This one works but analyze one instrument per time and the idea is to analyze all in parallel. That is to say send all requests to the server in parallel and wait for responses
@@ -22,7 +24,7 @@ async function techAnalysis() {
     console.log(JSON.stringify(returnArray))
 }
 
-// faster approach
+// faster approach with append file
 
 async function getDataFirstApproach() {
     if (fs.existsSync('./data.json')) {
@@ -42,9 +44,10 @@ async function getDataFirstApproach() {
     }
 }
 
-const DATE = '2020-09-09'
+// previous day date yyyy-mm-dd
+var date = moment().subtract(1, "days").format("YYYY-MM-DD");
 
-async function getData() {
+async function getData(date) {
     if (fs.existsSync('./data.json')) {
         fs.unlinkSync('./data.json')
     }
@@ -52,8 +55,8 @@ async function getData() {
     logger.info(`Attempting to process ${instrumentsArray.length} instruments`)
     for (const element of instrumentsArray) {
         let instumentAnalysis = []
-        ab.getMACDDifference(element, DATE).then((macd) => {
-            ab.getRSI(element, DATE).then((rsi) => {
+        ab.getMACDDifference(element, date).then((macd) => {
+            ab.getRSI(element, date).then((rsi) => {
                 instumentAnalysis.push({ symbol: element, RSI: rsi, MACD: macd })
                 fs.readFile('./data.json', function (err, data) {
                     var json = JSON.parse(data);
@@ -65,10 +68,12 @@ async function getData() {
     }
 }
 
-// getData()
+getData('2020-09-11')
 
-function AnalyzeData() {
+function getMACDTopGainers(top) {
     let differenceArray = []
+    let tops = []
+    let topsObjs = []
     var data = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
     logger.info(`Analyzing ${data.length} instruments`)
     data.forEach(element => {
@@ -76,12 +81,40 @@ function AnalyzeData() {
     });
     let sortedArray = differenceArray.sort()
     lessDifference = sortedArray[0]
-    const top_five = [sortedArray[0], sortedArray[1], sortedArray[2], sortedArray[3], sortedArray[4]]
+    for (let index = 0; index < top; index++) {
+        tops.push(sortedArray[index])
+    }
     data.forEach(element => {
-        if (top_five.includes(element[0].MACD[0].difference)) {
-            logger.info(`Top five selection:${JSON.stringify(element)}`)
+        if (tops.includes(element[0].MACD[0].difference)) {
+            logger.info(`Top ten selection:${JSON.stringify(element[0].symbol)}`)
             logger.info(element[0].MACD[0].difference)
+            topsObjs.push({ MACDDifference: element[0].MACD[0].difference, symbol: element[0].symbol, date: element[0].MACD[0].date })
         }
     });
+    return topsObjs
 }
-AnalyzeData()
+
+//getMACDTopGainers(5)
+
+function getRSITopGainers(top) {
+    let tops = []
+    let topsObjs = []
+    let data = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
+    let RSIArray = []
+    data.forEach(element => {
+        RSIArray.push(element[0].RSI[0].rsi)
+    });
+    let sortedArray = RSIArray.sort()
+    for (let index = 0; index < top; index++) {
+        tops.push(sortedArray[index])
+    }
+    data.forEach(element => {
+        if (tops.includes(element[0].RSI[0].rsi)) {
+            topsObjs.push({ rsi: element[0].RSI[0].rsi, symbol: element[0].symbol, date: element[0].RSI[0].date })
+        }
+    });
+    return topsObjs
+}
+
+// getRSITopGainers(20)
+// getMACDTopGainers(20)
