195b81efdb2938db9282177be0f2c4c8d7f8c844
Tomas Fleiderman <tomasfleiderman@xpertai.com>
2020-09-14 22:15:10 +0200

    update branch

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const AlphaVantage = require('../lib/AlphaVantage')
+const Marketstack = require('../lib/Marketstack')
 const fs = require('fs')
 const logger = require('../helper/logger')
 const moment = require('moment')
 
-ab = new AlphaVantage()
+// libertex lists
+// const instrumentsArray = ['TSLA','MRNA','NVDA','PINS','AMZN','SNAP','SPOT','AAPL','YNDX','NTDOY','FB','CRM','ADBE','APHA','LUV','EBAY','WSM','NFLX','BABA','SVMK','MSFT','SIE','ATVI','AA','NKE','RACE','DAI','CAT','SAP','GOOGL','RYAAY','DBK','TWTR','SQM','VALE','HD','ORCL','MA','DIS','SBUX','PG','EL','V','SK','RCL','UNH','GS','ETH','ADS','BIDU','MCD','BMW','F','VZ','HOG','UBER','MTS','TM','JNJ','PFE','RNO','BAC','CSCO','TRIP','DBX','VOW','BAS','AXP','VFC','BNP','IBM','JPM','OGZPY','CRON','ROSN','TRV','WYNN','KO','TIF','PVH','PM','DG','HPQ','INTC','BSAC','PTR','FP','CHL','LUKOY','ITX','C','ENIC','BAYN','PBR','GILD','XOM','LYFT','SAVE','VOD','T','KORS','AIR','RL','EDF','GE','CCL','NCLH','REP','WFC','AF','BA','TUI','ACB','TLRY','JWN','LTM']
 
-// const instrumentsArray = ['TSLA','MRNA','NVDA','PINS','AMZN','SNAP','SPOT','AAPL','YNDX','NTDOY','FB','CRM','ADBE','APHA','LUV','EBAY','WSM','NFLX','BABA','SVMK','MSFT','SIE','ATVI','TCTZ','AA','NKE','RACE','DAI','CAT','SAP','GOOGL','RYAAY','DBK','TWTR','SQM','VALE','HD','ORCL','NVTK','MA','DIS','SBUX','PG','EL','V','SK','RCL','UNH','GS','ETH','ADS','BIDU','MCD','JUVE','BMW','F','VZ','WEED','HOG','UBER','MTS','TM','JNJ','PFE','RNO','BAC','CSCO','LNVG','TRIP','DBX','VOW','SBER','BAS','AXP','VFC','BNP','IBM','JPM','AFLT','OGZPY','CRON','ROSN','ENEL','TRV','WYNN','KO','TIF','GMKN','PVH','PM','DG','HPQ','INTC','BSAC','PTR','FP','CHL','LUKOY','ITX','C','ENIC','BAYN','PBR','GILD','XOM','LYFT','SAVE','VOD','T','IDCB','KORS','ENI','AIR','RL','EDF','GE','CCL','NCLH','REP','WFC','AF','BA','TUI','ACB','TLRY','JWN','LTM']
+// const instrumentsArray = ['SBER', 'BAS', 'AXP', 'VFC', 'BNP', 'IBM', 'JPM', 'AFLT', 'OGZPY', 'CRON', 'ROSN', 'ENEL', 'TRV', 'WYNN', 'KO', 'TIF', 'GMKN', 'PVH', 'PM', 'DG', 'HPQ', 'INTC', 'BSAC', 'PTR', 'FP', 'CHL', 'LUKOY', 'ITX', 'C', 'ENIC', 'BAYN', 'PBR', 'GILD', 'XOM', 'LYFT', 'SAVE', 'VOD', 'T', 'IDCB', 'KORS', 'ENI', 'AIR', 'RL', 'EDF', 'GE', 'CCL', 'NCLH', 'REP', 'WFC', 'AF', 'BA', 'TUI', 'ACB', 'TLRY', 'JWN', 'LTM']
 
-const instrumentsArray = ['TSLA', 'MRNA', 'NVDA', 'PINS', 'AMZN', 'SNAP', 'SPOT', 'AAPL', 'YNDX', 'NTDOY', 'FB', 'CRM', 'ADBE', 'APHA', 'LUV', 'EBAY', 'WSM', 'NFLX', 'BABA', 'SVMK', 'MSFT', 'SIE', 'ATVI', 'TCTZ', 'AA', 'NKE', 'RACE', 'DAI', 'CAT', 'SAP', 'GOOGL', 'RYAAY', 'DBK', 'TWTR', 'SQM', 'VALE', 'HD', 'ORCL', 'NVTK', 'MA', 'DIS', 'SBUX', 'PG', 'EL', 'V', 'SK', 'RCL', 'UNH', 'GS', 'ETH', 'ADS', 'BIDU', 'MCD', 'JUVE', 'BMW', 'F', 'VZ', 'WEED', 'HOG', 'UBER', 'MTS', 'TM', 'JNJ', 'PFE', 'RNO', 'BAC', 'CSCO', 'LNVG', 'TRIP', 'DBX', 'VOW', 'SBER', 'BAS', 'AXP', 'VFC', 'BNP', 'IBM', 'JPM', 'AFLT', 'OGZPY', 'CRON', 'ROSN', 'ENEL', 'TRV', 'WYNN', 'KO', 'TIF', 'GMKN']
+const instrumentsArray = ['POM','KSS','TPR','HST']
 
 const returnArray = []
 
@@ -47,28 +49,22 @@ async function getDataFirstApproach() {
 // previous day date yyyy-mm-dd
 var date = moment().subtract(1, "days").format("YYYY-MM-DD");
 
-async function getData(date) {
+function getData(date) {
     if (fs.existsSync('./data.json')) {
         fs.unlinkSync('./data.json')
     }
-    fs.writeFileSync('./data.json', '[]')
+    fs.writeFileSync('./data.json', '')
     logger.info(`Attempting to process ${instrumentsArray.length} instruments`)
     for (const element of instrumentsArray) {
-        let instumentAnalysis = []
+        let instumentAnalysis = {}
         ab.getMACDDifference(element, date).then((macd) => {
             ab.getRSI(element, date).then((rsi) => {
-                instumentAnalysis.push({ symbol: element, RSI: rsi, MACD: macd })
-                fs.readFile('./data.json', function (err, data) {
-                    var json = JSON.parse(data);
-                    json.push(instumentAnalysis);
-                    fs.writeFileSync('./data.json', JSON.stringify(json))
-                })
+                instumentAnalysis = { symbol: element, RSI: rsi, MACD: macd }
+                fs.appendFileSync('./data.json', `${JSON.stringify(instumentAnalysis)},`)
             })
         })
     }
 }
-
-getData('2020-09-11')
 
 function getMACDTopGainers(top) {
     let differenceArray = []
@@ -77,24 +73,22 @@ function getMACDTopGainers(top) {
     var data = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
     logger.info(`Analyzing ${data.length} instruments`)
     data.forEach(element => {
-        differenceArray.push(element[0].MACD[0].difference)
+        differenceArray.push(element.MACD[0].difference)
     });
     let sortedArray = differenceArray.sort()
     lessDifference = sortedArray[0]
     for (let index = 0; index < top; index++) {
         tops.push(sortedArray[index])
     }
-    data.forEach(element => {
-        if (tops.includes(element[0].MACD[0].difference)) {
-            logger.info(`Top ten selection:${JSON.stringify(element[0].symbol)}`)
-            logger.info(element[0].MACD[0].difference)
-            topsObjs.push({ MACDDifference: element[0].MACD[0].difference, symbol: element[0].symbol, date: element[0].MACD[0].date })
-        }
+    tops.forEach(element => {
+        data.forEach(rawElement => {
+            if (rawElement.MACD[0].difference === element) {
+                topsObjs.push({ MACDdifference: rawElement.MACD[0].difference, symbol: rawElement.symbol, date: rawElement.MACD[0].date })
+            }
+        });
     });
     return topsObjs
 }
-
-//getMACDTopGainers(5)
 
 function getRSITopGainers(top) {
     let tops = []
@@ -102,19 +96,27 @@ function getRSITopGainers(top) {
     let data = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
     let RSIArray = []
     data.forEach(element => {
-        RSIArray.push(element[0].RSI[0].rsi)
+        RSIArray.push(element.RSI[0].rsi)
     });
     let sortedArray = RSIArray.sort()
     for (let index = 0; index < top; index++) {
         tops.push(sortedArray[index])
     }
-    data.forEach(element => {
-        if (tops.includes(element[0].RSI[0].rsi)) {
-            topsObjs.push({ rsi: element[0].RSI[0].rsi, symbol: element[0].symbol, date: element[0].RSI[0].date })
-        }
+
+    tops.forEach(element => {
+        data.forEach(rawElement => {
+            if (rawElement.RSI[0].rsi === element) {
+                topsObjs.push({ rsi: rawElement.RSI[0].rsi, symbol: rawElement.symbol, date: rawElement.RSI[0].date })
+            }
+        });
     });
     return topsObjs
 }
 
-// getRSITopGainers(20)
-// getMACDTopGainers(20)
+ab = new AlphaVantage()
+// m = new Marketstack()
+// m.getEOD()
+// console.log(getRSITopGainers(20))
+// console.log(getMACDTopGainers(20))
+getData('2020-09-11')
+// ab.getFundamental('POM')
